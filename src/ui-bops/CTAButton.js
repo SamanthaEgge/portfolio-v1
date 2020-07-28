4c623238d80e5ca2272a987740f7602e095483a1
Samantha Egge <samantha.c.egge@gmail.com>
2020-07-27 17:26:51 -0700

    Modified CTA Button styling. Need to fix width on it later. Looks fine as is now.

--- src/ui-bops/CTAButton.js
+++ src/ui-bops/CTAButton.js
@@ -6,7 +6,7 @@ const CTAButton = (props) => {
     return (
         <div 
             className={`cta-button ${props.size == 'medium' ? 'medium' : ''} ${props.color == 'secondary' ? 'secondary' : ''}`}>
-            {props.label}
+            <span>{props.label}</span>
         </div>
     )
 }
