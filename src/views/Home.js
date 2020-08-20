aab2bb3d78bbe1d0bea2d33515105a4969c004fb
Samantha Egge <samantha.c.egge@gmail.com>
2020-08-19 17:31:50 -0700

    Pulled the typing animation for now, in preparation for deployment.

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-import Typing from 'react-typing-animation'
+// import Typing from 'react-typing-animation'
+// TODO: Need to fix double print of typing animation while tabbing through the pages.
 
 import './Home.scss'
 import CTAButton from '../ui-bops/CTAButton.js'
@@ -24,8 +25,9 @@ class Home extends React.Component {
     return(
       <div className='home-container'>
         <div className='home-summary'>
-          <Typing.Delay ms={1000} />
-          <Typing speed={80}><h2>Hey, I'm Sam</h2></Typing>
+          {/* <Typing.Delay ms={1000} /> */}
+          {/* <Typing speed={80}><h2>Hey, I'm Sam</h2></Typing> */}
+          <h2>Hey, I'm Sam</h2>
           <h3>Creative Full Stack Web Developer looking to stay on the cutting edge of technology and work with a passionate team of engineers.</h3>
           <div className='header-buttons'>
             <Link to='/about'><CTAButton label={'Contact Me'} size={'large'} color={'primary'} /></Link>
